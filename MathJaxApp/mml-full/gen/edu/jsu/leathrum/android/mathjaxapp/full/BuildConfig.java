7823dd44bfb28a99a00ebdfc1617ebd7989d04df
justvisiting <hiteshchaudhary@gmail.com>
2014-04-08 23:45:00 -0700

    fix android new version

---
+++ MathJaxApp/mml-full/gen/edu/jsu/leathrum/android/mathjaxapp/full/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.jsu.leathrum.android.mathjaxapp.full;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
